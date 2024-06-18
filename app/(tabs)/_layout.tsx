81a77e3f42d8bd886bb6130e357608174299fbf3
murillobenicio <134696484+murillobenicio@users.noreply.github.com>
2024-06-18 15:15:03 +0000

    Forth_Commite

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,7 +19,7 @@ export default function TabLayout() {
         options={{
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon name={focused ? 'at-circle' : 'at-circle-outline'} color={color} />
           ),
         }}
       />
@@ -28,7 +28,7 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'apps' : 'apps-outline'} color={color} />
           ),
         }}
       />
